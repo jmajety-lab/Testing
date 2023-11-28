28909fb63936374372303d8d5e588bd059a60f91
Umesh Chandra <umeshchandra@wireless-10-105-58-85.umd.edu>
2023-11-27 19:46:22 -0500

    Pushing into github

--- skillmate/settings.py
+++ skillmate/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = 'django-insecure-&dgr^x)^#^jnys&p9g7djz&vd$1##vz3(c@1fbb3!c6yd^#f-#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["skillmate.up.railway.app"]
-
+# ALLOWED_HOSTS = ["skillmate.up.railway.app"]
+ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://skillmate.up.railway.app']
 
 CORS_ORIGIN_WHITELIST = [
