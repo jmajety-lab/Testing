7aef5e6985859d5e6069f07bf911fbc34e02e461
jitesh majety <36506374+jitesh98@users.noreply.github.com>
2023-11-30 18:39:15 -0500

    update

--- skillmate/urls.py
+++ skillmate/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# main admin paths
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
